2e40600e09fd614a2827c4ba392c35939311554a
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-05-02 21:38:33 -0300

    Update Onibus.java

--- Veiculo/src/Onibus.java
+++ Veiculo/src/Onibus.java
@@ -1,11 +1,11 @@
-public class Onibus extends Veiculo{
+public class Onibus extends VeiculoTerrestre{
     int capacidadePassageiros;
     int portasAcesso;
     int numeroParadas;
 
     public Onibus(String modelo, int ano, String cor, boolean ligado,
-                  int capacidadePassageiros, int portasAcesso, int numeroParadas) {
-        super(modelo, ano, cor, ligado);
+                  int numeroRodas, int capacidadePassageiros, int portasAcesso, int numeroParadas) {
+        super(modelo, ano, cor, ligado, numeroRodas);
         this.capacidadePassageiros = capacidadePassageiros;
         this.portasAcesso = portasAcesso;
         this.numeroParadas = numeroParadas;
