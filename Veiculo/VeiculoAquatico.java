dd6a765b655efa64fde941553d9aef7f2a46c862
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-05-02 21:36:58 -0300

    Delete Veiculo/VeiculoAquatico.java

--- Veiculo/VeiculoAquatico.java
+++
@@ -1,8 +0,0 @@
-public class VeiculoAquatico extends Veiculo{
-    float comprimento;
-
-    public VeiculoAquatico(String modelo, int ano, String cor, boolean ligado, float comprimento) {
-        super(modelo, ano, cor, ligado);
-        this.comprimento = comprimento;
-    }
-}
