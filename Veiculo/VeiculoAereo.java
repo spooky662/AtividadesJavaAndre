ac62898127bbe6d49bb9e4921bb34b0b8f4618d9
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-05-02 21:36:50 -0300

    Delete Veiculo/VeiculoAereo.java

--- Veiculo/VeiculoAereo.java
+++
@@ -1,9 +0,0 @@
-public class VeiculoAereo extends Veiculo{
-    int numeroMotores;
-
-    public VeiculoAereo(String modelo, int ano, String cor, boolean ligado, int numeroMotores) {
-        super(modelo, ano, cor, ligado);
-        this.numeroMotores = numeroMotores;
-    }
-
-}
