a5ef7b835d60128a8c9c1d5454f91ba4685a5e36
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-05-02 21:37:04 -0300

    Delete Veiculo/VeiculoTerrestre.java

--- Veiculo/VeiculoTerrestre.java
+++
@@ -1,8 +0,0 @@
-public class VeiculoTerrestre extends Veiculo{
-    int numeroRodas;
-
-    public VeiculoTerrestre(String modelo, int ano, String cor, boolean ligado, int numeroRodas) {
-        super(modelo, ano, cor, ligado);
-        this.numeroRodas = numeroRodas;
-    }
-}
